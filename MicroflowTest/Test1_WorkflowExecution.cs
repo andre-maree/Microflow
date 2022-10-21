4ce55964a9341efa86d6865bcf8307b8c7e717da
Andre <maree.andre@gmail.com>
2022-10-21 22:09:22 +0200

    Improved post data input on start

--- MicroflowTest/Test1_WorkflowExecution.cs
+++ MicroflowTest/Test1_WorkflowExecution.cs
@@ -38,7 +38,7 @@ namespace MicroflowTest
             Assert.IsTrue(successUpsert);
 
             // start the upserted Microflow
-            HttpResponseMessage startResult = await TestWorkflowHelper.StartMicroflow(microflow, loop, globalKey, "qweqwe");
+            HttpResponseMessage startResult = await TestWorkflowHelper.StartMicroflow(microflow, loop, globalKey);
 
             string instanceId = await WorkflowManager.WaitForWorkflowCompleted(startResult);
 
--- MicroflowTest/TestWorkflowHelper.cs
+++ MicroflowTest/TestWorkflowHelper.cs
@@ -149,28 +149,37 @@ namespace MicroflowTest
 
         public static Dictionary<string, string> CreateMergeFields(PassThroughParams passThroughParams, bool IsGet)
         {
-            string method = IsGet ? "get" : "post";
-            PropertyInfo[] props = passThroughParams.GetType().GetProperties();
-            string querystring = "?";
-            foreach (var param in props)
-            {
-                var val = param.GetValue(passThroughParams);
-                if ((bool)val == true)
-                {
-                    querystring += $"{param.Name}=<{param.Name}>&";
-                }
-            }
-            querystring = querystring.Remove(querystring.Length - 1);
-            //string querystring2 = "?WorkflowName=<WorkflowName>&MainOrchestrationId=<MainOrchestrationId>&SubOrchestrationId=<SubOrchestrationId>&Webhook=<Webhook>&RunId=<RunId>&StepNumber=<StepNumber>&GlobalKey=<GlobalKey>&StepId=<StepId>";
-
             Dictionary<string, string> mergeFields = new();
-            // use 
-            mergeFields.Add("default_post_url", $"https://reqbin.com/echo/{method}/json" + querystring);
-            // set the callout url to the new SleepTestOrchestrator http normal function url
-            //mergeFields.Add("default_post_url", baseUrl + "/SleepTestOrchestrator_HttpStart" + querystring);
-            //mergeFields.Add("default_post_url", baseUrl + "/testpost" + querystring);
 
-            return mergeFields;
+            if (IsGet)
+            {
+                PropertyInfo[] props = passThroughParams.GetType().GetProperties();
+                string querystring = "?";
+                foreach (var param in props)
+                {
+                    var val = param.GetValue(passThroughParams);
+                    if ((bool)val == true)
+                    {
+                        querystring += $"{param.Name}=<{param.Name}>&";
+                    }
+                }
+                querystring = querystring.Remove(querystring.Length - 1);
+                //string querystring2 = "?WorkflowName=<WorkflowName>&MainOrchestrationId=<MainOrchestrationId>&SubOrchestrationId=<SubOrchestrationId>&Webhook=<Webhook>&RunId=<RunId>&StepNumber=<StepNumber>&GlobalKey=<GlobalKey>&StepId=<StepId>";
+
+                // use 
+                mergeFields.Add("default_post_url", $"https://reqbin.com/echo/get/json" + querystring);
+                // set the callout url to the new SleepTestOrchestrator http normal function url
+                //mergeFields.Add("default_post_url", baseUrl + "/SleepTestOrchestrator_HttpStart" + querystring);
+                //mergeFields.Add("default_post_url", baseUrl + "/testpost" + querystring);
+
+                return mergeFields;
+            }
+            else
+            {
+                mergeFields.Add("default_post_url", $"https://reqbin.com/echo/post/json");
+
+                return mergeFields;
+            }
         }
     }
 }
